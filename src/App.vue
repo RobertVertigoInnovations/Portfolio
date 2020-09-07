bddc2f670a21642d16aed58dc957ccdd1e147df9
Robert Foster <robertfoster@vertigo-innovations.com>
2020-09-06 21:59:58 -0500

    first push

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,23 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <Navigation></Navigation>
   </div>
 </template>
+
+<script>
+import Navigation from './components/Navigation'
+export default {
+  name: 'app',
+  data() {
+    return {
+      key: value
+    }
+  },
+  components: {
+    Navigation: Navigation
+  }
+}
+</script>
 
 <style>
 #app {
@@ -15,6 +26,9 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  margin-top: -10px;
+  margin-left: -5px;
+  margin-right: -5px;
 }
 
 #nav {
---
+++ src/views/Projects.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="projects">
+    <p>Hello</p>
+  </div>
+</template>
