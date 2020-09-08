f2edfc36a29c1ea4e37ca3e3885fc148b4069440
Robert Foster <robertfoster@vertigo-innovations.com>
2020-09-07 20:35:42 -0500

    New Portfolio Page with updated bootstrap

---
+++ src/components/FullScaleProjects.vue
@@ -0,0 +1,85 @@
+<template>
+  <b-container fluid class="cardOverlay">
+    <b-row>
+      <b-col class="titleOverlay">
+        <h1>Full Scale Projects Projects</h1>
+      </b-col>
+    </b-row>
+
+    <b-row class="containerBox" align-h="between">
+      <b-col cols="4" id="projectListBox">
+        <b-col class="projectListBoxTitle">
+          <h2>Project List</h2>
+        </b-col>
+        <b-col cols="12" id="projectListBoxPh">
+          <projectList></projectList>
+        </b-col>
+      </b-col>
+      <b-col cols="7" id="projectDescriptionBox">
+        <b-col id="projectDescriptionBoxTitle">
+          <h2>Description</h2>
+        </b-col>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+import ProjectList from '@/components/ProjectList.vue'
+export default {
+  name: 'FullScaleProjects',
+  components: {
+    ProjectList: ProjectList
+  }
+}
+</script>
+
+<style>
+.cardOverlay {
+  border-width: 1px;
+  border-style: solid;
+  border-color: black;
+  color: white;
+}
+.containerBox {
+  background-color: rgb(6, 34, 128);
+  padding-bottom: 5%;
+}
+.titleOverlay {
+  background-color: rgb(22, 22, 22);
+  border-width: 5px;
+  border-style: solid;
+  border-color: rgb(4, 0, 59);
+}
+#projectListBox {
+  border-width: 5px;
+  border-style: solid;
+  border-color: rgb(4, 0, 59);
+  padding-left: 0;
+  padding-right: 0;
+  background-color: rgb(209, 215, 250);
+}
+.projectListBoxTitle {
+  background-color: rgb(22, 22, 22);
+  border-width: 3px;
+  border-style: solid;
+  border-color: rgb(4, 0, 59);
+}
+#projectListBoxPh {
+  padding-bottom: 65%;
+}
+#projectDescriptionBox {
+  border-width: 5px;
+  border-style: solid;
+  border-color: rgb(4, 0, 59);
+  padding-left: 0;
+  padding-right: 0;
+  background-color: rgb(209, 215, 250);
+}
+#projectDescriptionBoxTitle {
+  background: rgb(22, 22, 22);
+  border-width: 3px;
+  border-style: solid;
+  border-color: rgb(4, 0, 59);
+}
+</style>
---
+++ src/views/Projects.vue
@@ -0,0 +1,30 @@
+<template>
+  <!--<div class="projects">
+
+    <projectList></projectList>
+  </div>-->
+  <div class="projects">
+    <Cprojects></Cprojects>
+    <UIUXprojects></UIUXprojects>
+    <FullScaleProjects></FullScaleProjects>
+  </div>
+</template>
+<script>
+/*import ProjectList from '@/components/ProjectList.vue'*/
+import Cprojects from "@/components/Cprojects.vue";
+import UIUXprojects from "@/components/UIUXprojects.vue";
+import FullScaleProjects from "@/components/FullScaleProjects.vue";
+export default {
+  name: "Projects",
+  components: {
+    /* ProjectList */ Cprojects: Cprojects,
+    UIUXprojects: UIUXprojects,
+    FullScaleProjects: FullScaleProjects
+  }
+};
+</script>
+<style>
+.projects {
+  background-color: rgb(26, 26, 32);
+}
+</style>
